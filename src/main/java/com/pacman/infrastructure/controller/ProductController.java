0c86b5bc5ba5e34ff9eec37857b402728a00736c
sgmercader <sgmercader@minsait.com>
2025-07-15 20:57:12 +0200

    Insertar logs

--- src/main/java/com/pacman/infrastructure/controller/ProductController.java
+++ src/main/java/com/pacman/infrastructure/controller/ProductController.java
@@ -4,10 +4,11 @@ import com.pacman.domain.model.Product;
 import com.pacman.application.ProductService;
 import com.pacman.domain.ports.ProductRankingPort;
 import com.pacman.infrastructure.dto.WeightRequest;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-
+@Log4j2
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
@@ -21,6 +22,7 @@ public class ProductController {
     @PostMapping(value = "/sorted", produces = "application/json")
     public List<Product> getSortedProducts(@RequestBody WeightRequest weights) {
         Map<String, Double> weightsMap = new HashMap<>();
+        log.info("Received weights: {}", weights);
         if (weights.getSales() != null) weightsMap.put("sales", weights.getSales());
         if (weights.getStock_ratio() != null) weightsMap.put("stock_ratio", weights.getStock_ratio());
         return service.getSortedProducts(weightsMap);
